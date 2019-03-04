c8438041d9a64daa72a56fe06b3b8af3cc5a8ce9
NelsonKioko <kiokonelson2@gmail.com>
2019-03-04 15:44:54 +0300

    create buttons style them and add event listeners

--- js/script.js
+++ js/script.js
@@ -1,69 +1,92 @@
 
 
-var scores = [0,0];
+var finalTally = [0,0];//array with two values(score for player 1 and score for player 2)
 var total = 0;
-var activePlayer = 0;
+var currentPlayer = 0;
 var active = true;
 
 $(document).ready(function(){
-      document.querySelector('.player-0-panel').classList.add('active');
+  //add the string "active" to the div with class playerone
+document.querySelector('.player1').classList.add('active');
 
-$("#roll").click(function() {
+//create a rolldice function
+var rollDice = function() {
 
-    if (active) {
-        var randomNum = Math.floor(Math.random() * 6 + 1);
-        alert(randomNum);
-        if (randomNum !== 1) {
-            //Add score
-            total+=randomNum;
-            document.getElementById('player' + activePlayer).innerHTML = total;
-     } else {
-     nextPlayer();
-        }
-    }
-});
-});
-$(document).ready(function(){
-$("#hold").click(function() {
-    if (active) {
-        //Add current score to global score
-        scores[activePlayer] += total;
+  if (active) {
+      var randomNum = Math.floor(Math.random() * 6 + 1);
+      //Display the random number
+      document.getElementById('rolled' + currentPlayer).innerHTML = "you have rolled:" + randomNum;
+      //display you rolled a one please pass the dice
+      if (randomNum == 1) {
+         $("#output" + currentPlayer).show();
+          nextPlayer();
+       }
+      //Add values until player decides to hold or the turn ends
+      else if (randomNum !== 1) {
+          $("#output" + currentPlayer).hide();
+          total+=randomNum;
+          document.getElementById('total' + currentPlayer).innerHTML = total;
 
-        //Update the UI
-        document.getElementById('result' + activePlayer).innerHTML = scores[activePlayer];
+   } else {
+   nextPlayer();
+      }
+  }
+}
 
-        // Check if player won the game
-        if (scores[activePlayer] >= 100) {
-            document.getElementById("winner1").innerHTML = "You Won";
+//create a function for holding
+  var holdDice = function()  {
+  if (active) {
+      //Add current score to global score
+      finalTally[currentPlayer] += total;
 
-            active = false;
+      //Update the UI
 
-        } else {
-            nextPlayer();
-        }
-    }
-});
-});
+      document.getElementById('result' + currentPlayer).innerHTML = finalTally[currentPlayer];
 
+      // Check if player won the game
+      if (finalTally[currentPlayer] >= 100) {
+          document.getElementById("winner"+ currentPlayer).innerHTML = "You Won!!!!!";
 
-function nextPlayer() {
-    if (activePlayer === 0) {
-    activePlayer = 1;
+          active = false;
+
+      } else {
+      nextPlayer();
+      }
+  }
+}
+
+//Create a function to switch players
+var nextPlayer = function() {
+    if (currentPlayer === 0) {
+    currentPlayer = 1;
+    $(".player0").css("opacity", "0.9");
     }
     else {
-      activePlayer = 0;
+      currentPlayer = 0;
+      $(".player0").css("opacity", "0.9");
     }
 
     total = 0;
 
-    document.getElementById('player0').textContent = '0';
-    document.getElementById('player1').textContent = '0';
+    document.getElementById('total0').textContent = '0';
+    document.getElementById('total1').textContent = '0';
 
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.player0').classList.toggle('active');
+    document.querySelector('.player1').classList.toggle('active');
 
 
 }
 
-//var x = document.querySelector('#score-0').textContent;
-//console.log(x);
+
+
+$("#roll").click(function() {
+   rollDice();
+});
+
+
+$("#hold").click(function() {
+   holdDice();
+});
+
+});
